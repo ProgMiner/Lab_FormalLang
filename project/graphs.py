d0effb60942a981966a22a0ca440a0fa583420d4
Eridan Domoratskiy <eridan200@mail.ru>
2023-03-01 19:18:58 +0300

    Add composition functions

--- project/graphs.py
+++ project/graphs.py
@@ -12,7 +12,7 @@ def load_by_name(name: str) -> MultiDiGraph:
     return cfpq.graph_from_csv(cfpq.download(name))
 
 
-def summary(graph) -> GraphSummary:
+def summary(graph: MultiDiGraph) -> GraphSummary:
     return GraphSummary(
         nodes_amount=graph.number_of_nodes(),
         edges_amount=graph.number_of_edges(),
@@ -20,9 +20,17 @@ def summary(graph) -> GraphSummary:
     )
 
 
+def load_summary_by_name(name: str) -> GraphSummary:
+    return summary(load_by_name(name))
+
+
 def build_two_cycles(n: int, m: int, labels: Tuple[str, str]) -> MultiDiGraph:
     return cfpq.labeled_two_cycles_graph(n, m, labels=labels)
 
 
-def write_dot(graph, path):
+def write_dot(graph: MultiDiGraph, path):
     nx_pydot.write_dot(graph, path)
+
+
+def build_two_cycles_and_write_dot(n: int, m: int, labels: Tuple[str, str], path):
+    write_dot(build_two_cycles(n, m, labels), path)
