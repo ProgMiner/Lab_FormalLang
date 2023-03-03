a8cce2e7fa6e6aab1f81037a13e8d6994ca268f2
Eridan Domoratskiy <eridan200@mail.ru>
2023-03-03 14:04:41 +0300

    Add docstrings

--- project/graphs.py
+++ project/graphs.py
@@ -9,10 +9,18 @@ GraphSummary = namedtuple("GraphSummary", ["nodes_amount", "edges_amount", "labe
 
 
 def load_by_name(name: str) -> MultiDiGraph:
+    """
+    Загружает граф из датасета по имени.
+    """
+
     return cfpq.graph_from_csv(cfpq.download(name))
 
 
 def summary(graph: MultiDiGraph) -> GraphSummary:
+    """
+    Возвращает число вершин и рёбер в графе, а также множество меток.
+    """
+
     return GraphSummary(
         nodes_amount=graph.number_of_nodes(),
         edges_amount=graph.number_of_edges(),
@@ -21,16 +29,33 @@ def summary(graph: MultiDiGraph) -> GraphSummary:
 
 
 def load_summary_by_name(name: str) -> GraphSummary:
+    """
+    Возвращает сводную информацию для графа из датасета по его имени.
+    """
+
     return summary(load_by_name(name))
 
 
 def build_two_cycles(n: int, m: int, labels: Tuple[str, str]) -> MultiDiGraph:
+    """
+    Строит граф из двух циклов, в одном из которых n вершин, а в другом - m. Рёбра первого цикла
+    будут иметь метку первого элемента пары меток, рёбра второго - вторую.
+    """
+
     return cfpq.labeled_two_cycles_graph(n, m, labels=labels)
 
 
 def write_dot(graph: MultiDiGraph, path):
+    """
+    Записывает граф в формате DOT. В качестве файла может быть путь или объект файла.
+    """
+
     nx_pydot.write_dot(graph, path)
 
 
 def build_two_cycles_and_write_dot(n: int, m: int, labels: Tuple[str, str], path):
+    """
+    Строит граф из двух циклов и записывает в формате DOT.
+    """
+
     write_dot(build_two_cycles(n, m, labels), path)
