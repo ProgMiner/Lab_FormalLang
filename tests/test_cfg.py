8c29a4dce377d451ad12f5e8db4c5f57573c771b
Eridan Domoratskiy <eridan200@mail.ru>
2023-04-18 00:54:53 +0300

    Add tests

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -1,4 +1,5 @@
-from pyformlang.cfg import CFG
+from pyformlang.cfg import CFG, Variable
+import project.graphs as graphs
 import project.cfg as cfg
 import tempfile
 import re
@@ -76,3 +77,86 @@ S -> num
     expected_lines = set([l for l in expected.split("\n") if l != ""])
 
     assert actual_lines == expected_lines
+
+
+def test_hellings():
+    graph = graphs.build_two_cycles(1, 2, ("a", "b"))
+
+    grammar = CFG.from_text("S -> a S | epsilon")
+
+    assert cfg.hellings(graph, grammar) == {
+        (0, Variable("S"), 0),
+        (1, Variable("S"), 1),
+        (2, Variable("S"), 2),
+        (3, Variable("S"), 3),
+        (0, Variable("S"), 1),
+        (1, Variable("S"), 0),
+        (1, Variable("a#CNF#"), 0),
+        (0, Variable("a#CNF#"), 1),
+    }
+
+    grammar = CFG.from_text("S -> S b | epsilon")
+
+    assert cfg.hellings(graph, grammar) == {
+        (0, Variable("S"), 0),
+        (0, Variable("S"), 2),
+        (0, Variable("S"), 3),
+        (1, Variable("S"), 1),
+        (2, Variable("S"), 0),
+        (2, Variable("S"), 2),
+        (2, Variable("S"), 3),
+        (3, Variable("S"), 0),
+        (3, Variable("S"), 2),
+        (3, Variable("S"), 3),
+        (0, Variable("b#CNF#"), 2),
+        (2, Variable("b#CNF#"), 3),
+        (3, Variable("b#CNF#"), 0),
+    }
+
+    grammar = CFG.from_text("S -> a S b | epsilon")
+
+    assert cfg.hellings(graph, grammar) == {
+        (0, Variable("S"), 0),
+        (0, Variable("S"), 2),
+        (0, Variable("S"), 3),
+        (1, Variable("S"), 0),
+        (1, Variable("S"), 1),
+        (1, Variable("S"), 2),
+        (1, Variable("S"), 3),
+        (2, Variable("S"), 2),
+        (3, Variable("S"), 3),
+        (0, Variable("a#CNF#"), 1),
+        (1, Variable("a#CNF#"), 0),
+        (0, Variable("b#CNF#"), 2),
+        (2, Variable("b#CNF#"), 3),
+        (3, Variable("b#CNF#"), 0),
+        (0, Variable("C#CNF#1"), 0),
+        (0, Variable("C#CNF#1"), 2),
+        (0, Variable("C#CNF#1"), 3),
+        (1, Variable("C#CNF#1"), 0),
+        (1, Variable("C#CNF#1"), 2),
+        (1, Variable("C#CNF#1"), 3),
+        (2, Variable("C#CNF#1"), 3),
+        (3, Variable("C#CNF#1"), 0),
+    }
+
+
+def test_cfpq_hellings():
+    graph = graphs.build_two_cycles(1, 2, ("a", "b"))
+
+    grammar = CFG.from_text("S -> a S | epsilon")
+
+    assert cfg.cfpq_hellings(graph, grammar, [0]) == ({(0, 0), (0, 1)})
+
+    grammar = CFG.from_text("S -> S b | epsilon")
+
+    assert cfg.cfpq_hellings(graph, grammar, None, [0]) == {(0, 0), (3, 0), (2, 0)}
+
+    grammar = CFG.from_text("S -> a S b | epsilon")
+
+    assert cfg.cfpq_hellings(graph, grammar, [0, 1], [2, 3]) == {
+        (0, 2),
+        (0, 3),
+        (1, 2),
+        (1, 3),
+    }
