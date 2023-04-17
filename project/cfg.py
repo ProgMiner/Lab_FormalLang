5d0be31adc66a5f0bcab7d03c37b7f3b70bd07e9
Eridan Domoratskiy <eridan200@mail.ru>
2023-04-17 23:40:39 +0300

    Add Hellings implementation

--- project/cfg.py
+++ project/cfg.py
@@ -1,4 +1,5 @@
-from pyformlang.cfg import CFG
+from pyformlang.cfg import CFG, Epsilon, Terminal, Variable
+from networkx import MultiDiGraph
 
 
 def to_wcnf(cfg: CFG) -> CFG:
@@ -35,3 +36,76 @@ def from_file(path: str) -> CFG:
 
     with open(path) as f:
         return CFG.from_text(f.read())
+
+
+def hellings(graph: MultiDiGraph, cfg: CFG) -> set[tuple[int, Variable, int]]:
+    """
+    Hellings' algorithm got graph and context free grammar and returns a set of tuples
+    (vertex, nonterminal, vertex) so that from second vertex reachable from first by nonterminal.
+
+    In other words, it is edges of transitive closure of intersection of given graph and CFG.
+    """
+
+    cfg = to_wcnf(cfg)
+
+    r = set()
+    for p in cfg.productions:
+        body = p.body
+
+        print(body)
+
+        if len(p.body) == 0:
+            r |= {(v, name, v) for v in graph.nodes}
+
+        if len(p.body) != 1:
+            continue
+
+        name, term = p.head, p.body[0]
+
+        print(name, term)
+
+        if isinstance(term, Variable):
+            r |= {(v, name, u) for (v, u, l) in graph.edges(data="label") if l == term}
+
+    m = list(r)
+
+    while len(m) > 0:
+        v, ni, u = m.pop()
+
+        for v1, nj, u1 in r:
+            if u1 != v:
+                continue
+
+            for p in cfg.productions:
+                if len(p.body) != 2:
+                    continue
+
+                if p.body != [nj, ni]:
+                    continue
+
+                nk = p.head
+                if (v1, nk, u) in r:
+                    continue
+
+                m.append((v1, nk, u))
+                r.add((v1, nk, u))
+
+        for u1, nj, v1 in r:
+            if u1 != u:
+                continue
+
+            for p in cfg.productions:
+                if len(p.body) != 2:
+                    continue
+
+                if p.body != [ni, nj]:
+                    continue
+
+                nk = p.head
+                if (v, nk, v1) in r:
+                    continue
+
+                m.append((v, nk, v1))
+                r.add((v, nk, v1))
+
+    return r
