125de5d12dd181ed00348235f881f607cde23696
Eridan Domoratskiy <eridan200@mail.ru>
2023-05-26 18:24:54 +0300

    [WIP] Add tests for unary op, name and lambdas

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,3 +1,5 @@
+from project.fa import iterate_transitions
+from project.rfa import Nonterminal
 import project.interpreter as i
 from project.lang import parse
 import pytest
@@ -22,7 +24,19 @@ def check_value(value_type: type, value_value: any, value: i.LangValue):
 def check_value_type(value_type: type, value: i.LangValue):
     assert isinstance(value, value_type)
 
+    if isinstance(value, i.LangValueLambda):
+        return value.value
+
     return i.value_to_python_value(value)
+
+
+def match_pattern(pattern: str, value: i.LangValue):
+    match = i.interpret(parse(pattern, "pattern"))
+
+    result = dict()
+    match(result, value)
+
+    return result
 
 
 def test_empty_program():
@@ -171,19 +185,74 @@ def test_map_filter():
     )
 
 
-# def test_unary_op():
-#     assert l.check_syntax("set*;")
-#     assert l.check_syntax("set**/*wtf*/**;")
-#     assert l.check_syntax("not-set**/*wtf*/**;")
-#
-#     assert l.check_syntax("-1;")
-#     assert l.check_syntax("--- - - -  \n  1;")
-#     assert l.check_syntax("---- /* count them */ --- 1;")
-#     assert l.check_syntax("1 ---- /* next section :D */ ---- 1;")
-#
-#     assert l.check_syntax("not cond;")
-#     assert l.check_syntax("not not not cond;")
-#     assert l.check_syntax("not not /* not */ not not cond;")
+def test_unary_op():
+    result = check_value_type(i.LangValueFA, i.interpret(parse('"a"*', "expr")))
+
+    assert result.accepts("")
+    assert result.accepts("a")
+    assert result.accepts("aaa")
+
+    result = check_value_type(i.LangValueRSM, i.interpret(parse("rec a*", "expr")))
+
+    assert result.accepts([])
+    assert result.accepts([Nonterminal(x) for x in "a"])
+    assert result.accepts([Nonterminal(x) for x in "aaa"])
+
+    result = check_value_type(i.LangValueFA, i.interpret(parse('"a"***', "expr")))
+
+    assert result.accepts("")
+    assert result.accepts("a")
+    assert result.accepts("aaa")
+
+    result = check_value_type(i.LangValueRSM, i.interpret(parse("rec a***", "expr")))
+
+    assert result.accepts([])
+    assert result.accepts([Nonterminal(x) for x in "a"])
+    assert result.accepts([Nonterminal(x) for x in "aaa"])
+
+    check_value(i.LangValueInt, -1, i.interpret(parse("-1", "expr")))
+    check_value(i.LangValueInt, 1, i.interpret(parse("----1", "expr")))
+
+    check_value(i.LangValueReal, -0.21, i.interpret(parse("-0.21", "expr")))
+    check_value(i.LangValueReal, 0.21, i.interpret(parse("----0.21", "expr")))
+
+    with pytest.raises(i.InterpretError) as e:
+        i.interpret(parse('-"1"', "expr"))
+
+    assert (
+        str(e.value)
+        == "1:1: '1' created on 1:2 is of type string while (one of) ['int', 'real'] is expected"
+    )
+
+    check_value(i.LangValueBoolean, True, i.interpret(parse("not (1 != 1)", "expr")))
+    check_value(i.LangValueBoolean, False, i.interpret(parse("not (1 == 1)", "expr")))
+
+    check_value(
+        i.LangValueBoolean,
+        True,
+        i.interpret(parse("not not not not (1 == 1)", "expr")),
+    )
+    check_value(
+        i.LangValueBoolean,
+        False,
+        i.interpret(parse("not not not not (1 != 1)", "expr")),
+    )
+
+    with pytest.raises(i.InterpretError) as e:
+        i.interpret(parse("not 1", "expr"))
+
+    assert (
+        str(e.value)
+        == "1:1: 1 created on 1:5 is of type int while (one of) boolean is expected"
+    )
+
+    with pytest.raises(i.InterpretError) as e:
+        i.interpret(parse('not "a"', "expr"))
+
+    assert (
+        str(e.value)
+        == "1:1: 'a' created on 1:5 is of type string while (one of) boolean is expected"
+    )
 
 
 # def test_binary_op():
@@ -228,17 +297,25 @@ def test_map_filter():
 #     )
 
 
-# def test_name():
-#     assert l.check_syntax("test;")
-#     assert l.check_syntax("tEsT;")
-#     assert l.check_syntax("t_e_s_t;")
-#     assert l.check_syntax("test1;")
-#     assert l.check_syntax("test_1;")
-#     assert l.check_syntax("___test;")
-#     assert l.check_syntax("test___;")
-#     assert l.check_syntax("_;")
-#
-#     assert l.check_syntax("rec test;")
+def test_name():
+    assert "1\n" == interpret_to_str(parse("let test = 1; test;"))
+    assert "2\n" == interpret_to_str(parse("let tEsT = 2; tEsT;"))
+    assert "3\n" == interpret_to_str(parse("let t_e_s_t = 3; t_e_s_t;"))
+    assert "4\n" == interpret_to_str(parse("let test1 = 4; test1;"))
+    assert "5\n" == interpret_to_str(parse("let test_1 = 5; test_1;"))
+    assert "6\n" == interpret_to_str(parse("let ___test = 6; ___test;"))
+    assert "7\n" == interpret_to_str(parse("let test___ = 7; test___;"))
+    assert "8\n" == interpret_to_str(parse("let _ = 8; _;"))
+
+    with pytest.raises(i.InterpretError) as e:
+        assert interpret_to_str(parse("let test = 9; tEsT;"))
+
+    assert str(e.value) == '1:15: name "tEsT" is not in scope'
+
+    result = check_value_type(i.LangValueRSM, i.interpret(parse("rec test", "expr")))
+
+    assert result.accepts([Nonterminal("test")])
+    assert {(0, Nonterminal("test"), 1)} == set(iterate_transitions(result))
 
 
 def test_string():
@@ -315,33 +392,121 @@ def test_set():
     ]
 
 
-# def test_lambda():
-#     assert l.check_syntax("\\x->x;")
-#     assert l.check_syntax("\\ x -> x ;")
-#     assert l.check_syntax("\\ x -> x + y & x ;")
-#     assert l.check_syntax("\\ /* wtf */ x /* wtf */ -> /* wtf */ x in y & x ;")
-#
-#     # lambda consumes all expression
-#     assert (
-#         "([] ([14] ([31 14] ([43 31 14] \\ ([137 43 31 14] x) -> ([139 43 31 14] ([4 139 43 31 14] x) + ([59 139 43 31 14] ([4 59 139 43 31 14] y) & ([56 59 139 43 31 14] x)))))) ; <EOF>)"
-#         == l.parse("\\ x -> x + y & x ;").toStringTree()
-#     )
+def test_lambda():
+    value1 = i.interpret(parse("123", "literal"))
+    value2 = i.interpret(parse('"123"', "literal"))
+
+    result = check_value_type(
+        i.LangValueLambda,
+        i.interpret(parse(r"\x -> x", "literal")),
+    )
+    assert result(value1) is value1
+    assert result(value2) is value2
+
+    result = check_value_type(
+        i.LangValueLambda,
+        i.interpret(parse(r"\x -> x * 2", "literal")),
+    )
+    check_value(i.LangValueInt, 123 * 2, result(value1))
+    check_value(i.LangValueString, "123" * 2, result(value2))
+
+    result = check_value_type(
+        i.LangValueLambda,
+        i.interpret(parse(r"\x -> x + y & x", "literal")),
+    )
+
+    with pytest.raises(ValueError) as e:
+        result(value1)
+
+    assert str(e.value) == 'name "y" is not in scope'
 
 
-# def test_name_pattern():
-#     assert l.check_syntax("\\ xyz -> x;")
-#     assert l.check_syntax("\\ x123 -> x;")
-#     assert l.check_syntax("\\ x___123 -> x;")
-#     assert l.check_syntax("\\ _ -> x;")
+def test_name_pattern():
+    value = i.interpret(parse('"123"', "literal"))
+
+    result = match_pattern("xyz", value)
+    assert result == {"xyz": value}
+    assert result["xyz"] is value
+
+    result = match_pattern("x123", value)
+    assert result == {"x123": value}
+    assert result["x123"] is value
+
+    result = match_pattern("___123", value)
+    assert result == {"___123": value}
+    assert result["___123"] is value
+
+    assert match_pattern("_", value) == dict()
 
 
-# def test_tuple_pattern():
-#     assert l.check_syntax("\\ (x, y) -> x;")
-#     assert l.check_syntax("\\ (x, y, z) -> x;")
-#     assert l.check_syntax("\\ (x, y, z, w) -> x;")
-#     assert l.check_syntax("\\ (x, (y, z), w) -> x;")
-#     assert l.check_syntax("\\ (x, y, (z, w)) -> x;")
-#     assert l.check_syntax("\\ ((x, y), z) -> x;")
-#     assert l.check_syntax("\\ ((x,y),z) -> x;")
-#     assert l.check_syntax("\\ ((x, /* wtf */ y) /* lol */ ,z) -> x;")
-#     assert l.check_syntax("\\ (x, y, ) -> x;")
+def test_tuple_pattern():
+    ctx = parse("")
+
+    assert match_pattern("(x, y)", i.python_value_to_value(("test", 123), ctx)) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+    }
+
+    assert match_pattern(
+        "(x, y, z)",
+        i.python_value_to_value(("test", 123, 0.321), ctx),
+    ) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+        "z": i.LangValueReal(value=0.321, ctx=ctx),
+    }
+
+    assert match_pattern(
+        "(x, (y, z), w)",
+        i.python_value_to_value(("test", (123, 0.321), True), ctx),
+    ) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+        "z": i.LangValueReal(value=0.321, ctx=ctx),
+        "w": i.LangValueBoolean(value=True, ctx=ctx),
+    }
+
+    assert match_pattern(
+        "(x, y, (z, w))",
+        i.python_value_to_value(("test", 123, (0.321, True)), ctx),
+    ) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+        "z": i.LangValueReal(value=0.321, ctx=ctx),
+        "w": i.LangValueBoolean(value=True, ctx=ctx),
+    }
+
+    assert match_pattern(
+        "((x, y), z)",
+        i.python_value_to_value((("test", 123), 0.321), ctx),
+    ) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+        "z": i.LangValueReal(value=0.321, ctx=ctx),
+    }
+
+    assert match_pattern("(x, y, )", i.python_value_to_value(("test", 123), ctx)) == {
+        "x": i.LangValueString(value="test", ctx=ctx),
+        "y": i.LangValueInt(value=123, ctx=ctx),
+    }
+
+    with pytest.raises(ValueError) as e:
+        match_pattern("(x, y)", i.python_value_to_value("test", ctx))
+
+    assert (
+        str(e.value)
+        == "'test' created on 1:1 is of type string while (one of) tuple is expected"
+    )
+
+    with pytest.raises(ValueError) as e:
+        match_pattern("(x, y)", i.python_value_to_value(("test", 123, 0.321), ctx))
+
+    assert str(e.value) == "wrong number of elements in tuple 3 vs expected 2"
+
+    with pytest.raises(ValueError) as e:
+        match_pattern("(x, (y, z))", i.python_value_to_value(("test", 123), ctx))
+
+    assert (
+        str(e.value)
+        == "123 created on 1:1 is of type int while (one of) tuple is expected"
+    )
