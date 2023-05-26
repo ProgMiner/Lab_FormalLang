60da29676d8d383b85454a4b1fd040a8f803cc03
Eridan Domoratskiy <eridan200@mail.ru>
2023-05-26 18:24:54 +0300

    [WIP] Add tests for unary op, name and lambdas

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -2,11 +2,17 @@
 
 import subprocess
 import shared
+import sys
 
 
 def main():
     shared.configure_python_path()
-    subprocess.check_call(["python", "-m", "pytest", "-vv", "-s", shared.TESTS])
+
+    if len(sys.argv) == 2:
+        subprocess.check_call(["pytest", "-vv", "-s", sys.argv[1]])
+
+    else:
+        subprocess.check_call(["pytest", "-vv", "-s", shared.TESTS])
 
 
 if __name__ == "__main__":
